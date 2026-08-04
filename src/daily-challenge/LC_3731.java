7885f65544823b78b4f88937389a4adef6da5d95
Arjun Singh <arjunsingh31400@gmail.com>
2026-08-04 11:56:47 +0100

    feat: LC #3731. Find Missing Elements

---
+++ src/daily-challenge/LC_3731.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        Set<Integer> set = new HashSet<>();
+        int min = nums[0]; 
+        int max = nums[0];
+
+        for(int num : nums){
+            set.add(num);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        for(int i = min; i<= max; i++){
+            if(!set.contains(i)){
+                result.add(i);
+            }
+        }
+        return result;
+        
+    }
+}
